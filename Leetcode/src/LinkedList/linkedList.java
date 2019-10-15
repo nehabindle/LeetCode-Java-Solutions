b74bb668c5d60b43b29454e70afdf7054eac4aab
neha <neha@localhost>
2019-10-15 13:04:40 -0700

    First commit

---
+++ Leetcode/src/LinkedList/linkedList.java
@@ -0,0 +1,26 @@
+package LinkedList;
+import java.util.*;
+
+
+public class linkedList {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> obj = new LinkedList<String>();
+		
+		    obj.addFirst("A");
+		    obj.add("B");
+		    obj.add("C");
+		    obj.add("E");
+		    
+		    obj.addLast("F");
+		    
+        System.out.println("Linked List : " +obj);
+ 
+        
+            obj.add(5,"G");
+            
+            System.out.println("Linked List : " +obj);
+	}
+
+}
