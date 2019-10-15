97d46e6085d4b68f6ba573cecbe27911a88bce16
neha <neha@localhost>
2019-10-14 19:39:49 -0700

    Second Commit

--- Leetcode/src/q_1/ArraySum.java
+++ Leetcode/src/q_1/ArraySum.java
@@ -5,6 +5,8 @@ public class ArraySum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Git Commit");
+		System.out.println("Second Git Commit");
+
 
 	}
 
