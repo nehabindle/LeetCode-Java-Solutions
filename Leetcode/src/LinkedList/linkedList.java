d8edfae1a3aef86de5c2c48acfe1c963cdcc06e3
neha <neha@localhost>
2019-10-15 18:53:44 -0700

    intial

--- Leetcode/src/LinkedList/linkedList.java
+++ Leetcode/src/LinkedList/linkedList.java
@@ -23,6 +23,32 @@ public class linkedList {
         System.out.println("Linked List : " +obj);
             
             
+        	obj.remove("B");
+        	obj.remove(3);
+        
+        System.out.println("Linked List : " +obj);
+        
+       boolean status = obj.contains("F");
+        
+        if(status)
+        {
+        	System.out.println("It contains the required element");
+        	
+        }
+   
+        else
+        {
+        	System.out.println("It doesn't contain the element");
+        	
+        }
+        
+        int List_size;
+        
+       List_size = obj.size();
+       System.out.println(List_size);
+       
+       obj.set(3, "U");
+       System.out.println("Linked List : " +obj);
             
 	}
 
