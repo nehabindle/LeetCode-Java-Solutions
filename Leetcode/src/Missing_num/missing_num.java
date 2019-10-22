e1f30d8e5aa0ca1fd3cd3b8d1bb55ed7296b6ecf
neha <neha@localhost>
2019-10-22 16:49:25 -0700

    Leetcode

--- Leetcode/src/Missing_num/missing_num.java
+++ Leetcode/src/Missing_num/missing_num.java
@@ -6,15 +6,25 @@ public class missing_num {
 		 int n = nums.length;
 		int total_sum = (n*(n+1))/2;
 		
+		 if(nums[n-1]!=n)
+	        {
+	            return n;
+	        }
+	        else if (nums[0]!=0){
+	            return 0;
+	        } 
+	    else{  
 		for(int i=0; i<n;i++)
+		{
 			sum += nums[i];
+		}
 		return total_sum - sum;
 	 }
-	
+	 }
 
 	public static void main(String[] args) {
 		
-		int[] num = {0,2,3,4,5,6,7,8,9,10};
+		int[] num = {0,2,3,4,6,5,7,9,8,10,11,12};
 		
 		missing_num obj = new missing_num();
 		
